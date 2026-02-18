58c53089e90e97c1dcd521398832cb90c24371e4
Kundan Chaudhary <kundanofficial2005@gmail.com>
2026-02-18 19:47:17 +0000

    container config done

---
+++ Basics/one.js
@@ -0,0 +1,22 @@
+const accountId=12412699
+let accountEmail="kd123@gmail.com"
+
+var accountPass="12345"
+accountCity="Delhi"
+
+//accountId=2
+
+console.table([accountId,accountEmail,accountPass.accountCity])
+
+console.log(accountId);
+accountEmail="sh143@gmail.com"
+accountPass="9798"
+accountCity="Punjab"
+
+let accountState;
+// console.log(accountEmail);
+// console.log(accountPass);
+// console.log(accountCity);
+
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
